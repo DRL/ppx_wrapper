6648b8152961f4a9ba3725071b34bf29e801bd5c
Dominik R Laetsch <dominik.laetsch@gmail.com>
2015-03-05 18:37:10 +0000

    ...

--- ppx_wrapper.py
+++ ppx_wrapper.py
@@ -65,8 +65,7 @@ def run_fastblocksearch(contig):
 def fastblocksearch(contigs):
 	print str(len(contigs)) + " contigs"
 	pool = mp.Pool(processes=10)
-	for contig in contigs:
-		results = pool.apply(run_fastblocksearch, contig)
+	results = [pool.apply_async(run_fastblocksearch, args=(contig,) for contig in contigs)]
 	# pool = mp.Pool(processes=10)
 	# results = [pool.apply_async(run_fastblocksearch, args=(profile_file, contig.header, contig.seq)) for contig in contigs]
 	#output = [p.get() for p in results]
