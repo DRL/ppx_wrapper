8ec3e342cd4ec8b23bed0322fa5cf0205748a12e
Dominik R Laetsch <dominik.laetsch@gmail.com>
2015-03-09 15:07:11 +0000

    ...

--- ppx_wrapper.py
+++ ppx_wrapper.py
@@ -168,7 +168,6 @@ def runAugustusPPX():
 			list_of_blocks = parseFastBlockSearchResult(result_file)
 
 			for block in list_of_blocks:
-				print block.__dict__
 				dict_of_blocks[block.profile][block.score] = block
 
 	contig, start, end, strand, score, profile = selectBestBlock(dict_of_blocks)
