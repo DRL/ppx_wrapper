d41ee27009a2b73ed7c8537d208caf89fc6a4f5d
Dominik R Laetsch <dominik.laetsch@gmail.com>
2015-03-09 14:20:49 +0000

    ...

--- ppx_wrapper.py
+++ ppx_wrapper.py
@@ -171,7 +171,7 @@ def runAugustusPPX():
 
 	contig, start, end, strand, score = selectBestBlock(dict_of_blocks)
 	infile = TEMP_DIR + contig + ".temp"
-	outfile = AUGUSTUS_DIR + contig + + ".gff3"
+	outfile = AUGUSTUS_DIR + contig + ".gff3"
 	print "[STATUS] - Calling protein \"" + query + "\" in contig \"" + contig + "\" from " + str(start) + " to " + str(end)  
 	process = subprocess.Popen("/exports/software/augustus/augustus-3.0.3/bin/augustus --species=caenorhabditis --gff3=on --proteinprofile=" + profile + " --predictionStart=" + start + " --predictionEnd=" + end + " --strand=" + strand + " " + infile + " > " + outfile , stderr=subprocess.PIPE, stdout=subprocess.PIPE, shell=True)
 	process.wait()
