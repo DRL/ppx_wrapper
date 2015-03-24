f4f5bb01234f2bf6bb9d3424f1eb9fdf05d1f86f
Dominik R. Laetsch <dominik.laetsch@gmail.com>
2015-03-24 22:57:43 +0000

    ...

--- ppx_wrapper.py
+++ ppx_wrapper.py
@@ -127,9 +127,8 @@ def fastblocksearch(profile, contigs):
 		i += 1
 		temp_file = contigs[contig]
 		out_file = FASTBLOCKSEARCH_DIR + contig + "." + profile.split("/")[-1].split(".")[0] + ".result"
-		if i % 200 == 0:
-			print i
-			time.sleep(10)
+		if i % 100 == 0:
+			time.sleep(20)
 		process = subprocess.Popen("/exports/software/augustus/augustus-3.0.3/bin/fastBlockSearch --cutoff=0.5 " + temp_file + " " + profile + " > " + out_file + " ", stderr=subprocess.PIPE, stdout=subprocess.PIPE, shell=True)
 		processes.append(process)
 
