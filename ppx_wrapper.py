5ce6adfd5aaff8493809db3241ecfadad23fc9b8
Dominik R. Laetsch <dominik.laetsch@gmail.com>
2015-05-16 20:31:33 +0100

    ...

--- ppx_wrapper.py
+++ ppx_wrapper.py
@@ -121,6 +121,7 @@ def fastblocksearch(profile, contigs):
 		temp_file = contigs[contig]
 		out_file = FASTBLOCKSEARCH_DIR + contig + "." + profile.split("/")[-1].split(".")[0] + ".result"
 		cmd = "/exports/software/augustus/augustus-3.0.3/bin/fastBlockSearch --cutoff=0.5 " + temp_file + " " + profile + " > " + out_file + " "
+		prtin cmd
 		jobs.append(cmd)
 
 	results = run_jobs(jobs, 24, pause = 2, verbose = False)
@@ -214,7 +215,7 @@ def analyseBlocks(dict_of_blocks):
 	for score in sorted(dict_of_blocks, reverse=True):
 		# for every score (in decreasing numerical order)
 		
-		# print str(score) + "\t" + str(dict_of_blocks[score].__dict__) # for debugging
+		print str(score) + "\t" + str(dict_of_blocks[score].__dict__) # for debugging
 		
 		block = dict_of_blocks[score] # get the block
 
